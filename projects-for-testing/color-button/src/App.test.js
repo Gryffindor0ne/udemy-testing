5f81a003ba5fd5b6cb03dacbc609f76e792d1cdd
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2023-01-26 11:20:07 +0900

    Feat:Test color button click

--- projects-for-testing/color-button/src/App.test.js
+++ projects-for-testing/color-button/src/App.test.js
@@ -1,13 +1,15 @@
-import { render, screen } from "@testing-library/react";
-import { logRoles } from "@testing-library/dom";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("button has correct initial color", () => {
-  const { container } = render(<App />);
-  logRoles(container);
+test("button has correct initial color and updates when clicked", () => {
+  render(<App />);
 
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
   expect(colorButton).toHaveStyle({ backgroundColor: "red" });
-});
 
-test("button turns blue when clicked", () => {});
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  expect(colorButton.textContent).toBe("Change to red");
+});
