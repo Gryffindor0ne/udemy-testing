7a66366471d52afaaa2c39fb30b104e2551d259f
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2023-02-03 11:24:23 +0900

    Feat: popover test

--- projects-for-testing/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ projects-for-testing/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -32,3 +32,21 @@ test("Checkbox disables button on first click and enable on second click", async
   await user.click(checkbox);
   expect(confirmOrderButton).toBeDisabled();
 });
+
+test("popover responds to hover", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
+});
