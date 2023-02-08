605700ecb81b642b7bb76150c2aa83f70687285b
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2023-02-08 11:27:01 +0900

    Feat: topping test

--- projects-for-testing/sundaes-on-demand/src/pages/entry/test/Options.test.jsx
+++ projects-for-testing/sundaes-on-demand/src/pages/entry/test/Options.test.jsx
@@ -11,3 +11,19 @@ test("displays image for each scoop option from server", async () => {
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("displays image for toppings option", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  const imageTitles = toppingImages.map((img) => img.alt);
+  expect(imageTitles).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
